213b79bfc383d4dee88c8157e2de2bca3e6358b2
Stanislav <stas.ace@gmail.com>
2024-01-28 21:44:40 +0500

    Setting UI (failed)

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    
+    private var currentColor = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        redView.layer.cornerRadius = 50
+        yellowView.layer.cornerRadius = 50
+        greenView.layer.cornerRadius = 50
+        
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func playButton(_ sender: UIButton) {
+        
+        switch currentColor {
+        case "red":
+            redView.alpha = 0.3
+            yellowView.alpha = 1
+            currentColor = "yellow"
+        case "yellow":
+            yellowView.alpha = 0.3
+            greenView.alpha = 1
+            currentColor = "green"
+        case "green":
+            greenView.alpha = 0.3
+            redView.alpha = 1
+            currentColor = "red"
+        default:
+            currentColor = "red"
+            redView.alpha = 1
+            sender.setTitle("Next", for: .normal)
+        }
+    }
+    
 }
 
